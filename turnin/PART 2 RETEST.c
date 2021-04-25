c57c9d4ddaa8b10ea038ea03ec1c0db771d36d84
EmilyRomero19 <76976095+EmilyRomero19@users.noreply.github.com>
2021-04-25 14:07:43 -0700

    Update PART 2 RETEST.c

--- turnin/PART 2 RETEST.c
+++ turnin/PART 2 RETEST.c
@@ -28,13 +28,130 @@ void Tick_Reset(){
 	case SM1_INIT1:
 		if(button == 0x01){
 			SM1_STATE = SM1_T2;
-			
+			hold = 1;
 		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else if(button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_INIT1;
+		}
+		break;
+			
+	case SM1_T2:
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+		}
+		else if(button == 0x02 || button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_T3;
+		}
+		break;
+			
+	case SM1_T3:
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+			hold = 1;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else if(button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_T3;	
+		}
+		break;
+			
+	case SM1_T1:
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+			hold = 1;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else if(button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_T1;	
+		}
+		break;
 		
+	case SM1_T4:
+		if(button == 0x01 || button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+		}
+		else{
+			SM1_STATE = SM1_T1;	
+		}
+		break;
+	
+	case SM1_RESET:	
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+			hold = 1;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else{
+			SM1_STATE = SM1_RESET;	
+		}
+		break;	
 			
-			
-
+	 default:
+		SM1_STATE = SM1_SMStart;
+		
         }
+	
+	switch(SM1_STATE){
+			
+	case SM1_SMStart:
+	break;
+	
+	case SM1_INIT1:
+	PORTC = 0x07;
+	break;
+			
+	case SM1_T2:
+	if (PINC < 9 && hold == 1){
+	PORTC = PORTC + 1;
+	hold = 0;
+	}
+	break;
+			
+	case SM1_T2:
+	break;
+	
+	case SM1_T1:
+	break;
+		
+	case SM1_T4:
+	if (PINC > 0 && hold == 1){
+	PORTC = PORTC - 1;
+	hold = 0;
+	}
+	break;
+			
+	dafault:
+	break;
+			
+	}
 }
 
 int main(void) {
