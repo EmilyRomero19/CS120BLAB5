4caa2fc2bd0f0edb49e300248497cba13eb8183c
EmilyRomero19 <76976095+EmilyRomero19@users.noreply.github.com>
2021-04-25 14:21:04 -0700

    Update erome035_lab5_part2.c

--- turnin/erome035_lab5_part2.c
+++ turnin/erome035_lab5_part2.c
@@ -1,5 +1,4 @@
-
- /*	Author: erome035
+/*	Author: erome035
  *  Partner(s) Name: 
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
@@ -14,116 +13,147 @@
 #include "simAVRHeader.h"
 #endif
 
-enum SM1_STATES { SM1_SMStart, SM1_INIT1, SM1_INIT2, SM1_ADD1, SM1_MINUS1, SM1_ADD, SM1_MINUS, SM1_RESET } SM1_STATE;
-
-//modify old code to be able to simulate on microcontroller 
-
-void Tick_Reset() {	
+enum SM1_STATES { SM1_SMStart, SM1_INIT1, SM1_T1, SM1_T2, SM1_T3, SM1_T4, SM1_RESET } SM1_STATE;
+unsigned char hold = 0;
+void Tick_Reset(){
 	
-	//unsigned char ButtonAdd = PINA & 0x01;  DONT NEED 
-	//unsigned char ButtonMinus = PINA & 0x02; DONT NEED 
+	button = ~PINA & 0x01; // button is connected to A0
+	
 	switch(SM1_STATE){
-		
+			
 	case SM1_SMStart:
 		SM1_STATE = SM1_INIT1;
 		break;
-		
+			
 	case SM1_INIT1:
-		SM1_STATE = SM1_INIT2;
-		break;
-			
-	case SM1_INIT2: //MODIFY USING ~PINA for microcontroller 
-		if ((~PINA & 0x03) == 0x03) {
-                    SM1_STATE = SM1_RESET;
-                }
-		else if ((~PINA & 0x01) == 0x01) {
-		     SM1_STATE = SM1_ADD;
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+			hold = 1;
 		}
-		else if ((~PINA & 0x02) == 0x02) {
-                    SM1_STATE = SM1_MINUS;
-                }
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else if(button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_INIT1;
+		}
+		break;
+			
+	case SM1_T2:
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+		}
+		else if(button == 0x02 || button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_T3;
+		}
+		break;
+			
+	case SM1_T3:
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+			hold = 1;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else if(button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_T3;	
+		}
+		break;
+			
+	case SM1_T1:
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+			hold = 1;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else if(button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else{
+			SM1_STATE = SM1_T1;	
+		}
+		break;
+		
+	case SM1_T4:
+		if(button == 0x01 || button == 0x03){
+			SM1_STATE = SM1_RESET;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+		}
+		else{
+			SM1_STATE = SM1_T1;	
+		}
 		break;
 	
-	case SM1_ADD1:
-		if ((~PINA & 0x03) == 0x01) {
-                      SM1_STATE = SM1_ADD1;
-                }
-                else {
-                     SM1_STATE = SM1_INIT2;
-                }
-                break;
-			
-	case SM1_ADD:	
-		SM1_STATE = SM1_ADD1;
-		break;
-	
-	case SM1_MINUS1:		
-		 if ((~PINA & 0x03) == 0x02) {
-                     SM1_STATE = SM1_MINUS1;
-                 }
-                 else {
-                      SM1_STATE = SM1_INIT2;
-                 }
-                 break;
-			
-	case SM1_MINUS:	
-		SM1_STATE = SM1_MINUS1;
+	case SM1_RESET:	
+		if(button == 0x01){
+			SM1_STATE = SM1_T2;
+			hold = 1;
+		}
+		else if(button == 0x02){
+			SM1_STATE = SM1_T4;
+			hold = 1;
+		}
+		else{
+			SM1_STATE = SM1_RESET;	
+		}
 		break;	
 			
-	case SM1_RESET:
-		if ((~PINA & 0x03) == 0x03) {
-                      SM1_STATE = SM1_RESET;
-                }
-                else {
-                      SM1_STATE = SM1_INIT2;
-                }
-                break;
+	 default:
+		SM1_STATE = SM1_SMStart;
+		
+        }
 	
-	default:
-	SM1_STATE = SM1_SMStart;
-	break;		
-					
-	}
-	switch(SM1_STATE) {
-	case SM1_SMStart:
-	PORTC = 0x07;
-	break;
+	switch(SM1_STATE){
 			
+	case SM1_SMStart:
+	break;
+	
 	case SM1_INIT1:
 	PORTC = 0x07;
 	break;
 			
-	case SM1_INIT2:
-	break;
-			
-	case SM1_ADD1: // SWITCH FROM LAST TIME wrong output
-	break;
-			
-	case SM1_MINUS1: //SWITCH FROM LAST TIME
-	break;
-			
-	case SM1_ADD: //SWITCH FROM LAST TIME
-	if (PORTC < 0x09) {
-        PORTC = PORTC + 1;
+	case SM1_T2:
+	if (PINC < 9 && hold == 1){
+	PORTC = PORTC + 1;
+	hold = 0;
 	}
-        break;
-			
-	case SM1_MINUS://SWITCH FROM LAST TIME
-	if (PORTC > 0x00) { 
-        PORTC = PORTC - 1;
-	}
-        break;
-			
-	case SM1_RESET:
-	PORTC = 0x00;
 	break;
 			
-	default:
-	PORTC = 0x07;
+	case SM1_T2:
 	break;
-	}	
-}
 	
+	case SM1_T1:
+	break;
+		
+	case SM1_T4:
+	if (PINC > 0 && hold == 1){
+	PORTC = PORTC - 1;
+	hold = 0;
+	}
+	break;
+			
+	dafault:
+	break;
+			
+	}
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 
